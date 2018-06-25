f2cc8083bfb36c246dbd214b7fdde628cb2a526f
alc29 <decay@alc29s-iMac.local>
2018-06-24 18:36:14 -0700

    add empty xcode project

---
+++ src/NutritionTracker/NutritionTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-24.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ src/NutritionTracker/NutritionTrackerTests/NutritionTrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  NutritionTrackerTests.swift
+//  NutritionTrackerTests
+//
+//  Created by alc29 on 2018-06-24.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import XCTest
+@testable import NutritionTracker
+
+class NutritionTrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
