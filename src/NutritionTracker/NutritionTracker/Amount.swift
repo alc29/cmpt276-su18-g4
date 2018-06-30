204afe075f11becf550dfbd6316da37f98181fca
alc29 <alc29@sfu.ca>
2018-06-29 23:00:13 -0700

    add Amount.swift

---
+++ src/NutritionTracker/NutritionTracker/Amount.swift
@@ -0,0 +1,50 @@
+//
+//  Amount.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-29.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+
+class Amount: Object {
+	@objc private var amount: Float = 0.0
+	@objc private var unit = Unit.Miligram.rawValue
+
+	convenience init(_ amount: Float? = 0.0, _ unit: Unit? = Unit.Miligram) {
+		self.init()
+		
+		if (amount != nil) {
+			if (amount! >= 0) {
+				self.amount = amount!
+			}
+		}
+		
+		if (unit != nil) {
+			self.unit = unit!.rawValue
+		}
+	}
+	
+	// MARK: Setters
+	func setAmount(_ amount: Float) {
+		if (amount >= 0) {
+			self.amount = amount
+		}
+	}
+	
+	func setUnit(_ unit: Unit) {
+		self.unit = unit.rawValue
+	}
+	
+	//MARK: Getters
+	func getAmount() -> Float {
+		return amount
+	}
+	func getUnit() -> Unit {
+		return Unit(rawValue: unit)!
+	}
+	
+}
---
+++ src/NutritionTracker/NutritionTracker/Unit.swift
@@ -0,0 +1,9 @@
+//
+//  Unit.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-29.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import Foundation
