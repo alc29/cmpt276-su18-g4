81348dcebea2f543e7fc9dd562382fd31f8502c3
alc29 <alc29@sfu.ca>
2018-06-29 17:50:10 -0700

    add FoodItemList.swift

---
+++ src/NutritionTracker/NutritionTracker/FoodItemList.swift
@@ -0,0 +1,82 @@
+//
+//  FoodItemList.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-29.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class FoodItemList: Object {
+	@objc private dynamic var id = UUID().uuidString
+	//@objc private dynamic var array = [FoodItem]()
+	let list = List<FoodItem>()
+	
+	func add(_ foodItem: FoodItem) {
+		list.append(foodItem)
+	}
+
+//	func remove(_ index: Int) -> FoodItem? {
+//		let count = list.count;
+//		if (count > 0 && index >= 0 && index < count) {
+//			return list.remove(at: index)
+//		}
+//		return nil;
+//	}
+	func remove(_ index: Int) {
+		let count = list.count;
+		if (count > 0 && index >= 0 && index < count) {
+			list.remove(at: index)
+		}
+	}
+	
+	func count() -> Int {
+		return list.count;
+	}
+
+	func validIndex(_ i: Int) -> Bool {
+		let count = self.count()
+		return (count > 0 && i >= 0 && i < count);
+	}
+
+	func get(_ index: Int) -> FoodItem? {
+		if self.validIndex(index) {
+			return list[index];
+		}
+		return nil;
+	}
+	
+//	func add(_ foodItem: FoodItem) {
+//		array.append(foodItem)
+//	}
+//
+//	func remove(_ index: Int) -> FoodItem? {
+//		let count = array.count;
+//		if (count > 0 && index >= 0 && index < count) {
+//			return array.remove(at: index)
+//		}
+//		return nil;
+//	}
+//
+//	func count() -> Int {
+//		return array.count;
+//	}
+//
+//	func validIndex(_ i: Int) -> Bool {
+//		let count = self.count()
+//		return (count > 0 && i >= 0 && i < count);
+//	}
+//
+//	func get(_ index: Int) -> FoodItem? {
+//		if self.validIndex(index) {
+//			return array[index];
+//		}
+//		return nil;
+//	}
+	
+	override static func primaryKey() -> String? {
+		return "id";
+	}
+}
