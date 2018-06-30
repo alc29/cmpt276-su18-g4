1e568ee3c2732d968129acdf6877a7b3dc77a5fd
alc29 <alc29@sfu.ca>
2018-06-30 12:00:50 -0700

    add Category TableView & Cell classes

---
+++ src/NutritionTracker/NutritionTracker/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-30.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
