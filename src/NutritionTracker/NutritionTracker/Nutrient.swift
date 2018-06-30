c66e5f7ea813473a9bb08ab68834faaba5662a81
alc29 <alc29@sfu.ca>
2018-06-30 16:39:40 -0700

    add Nutrient.swift

---
+++ src/NutritionTracker/NutritionTracker/Nutrient.swift
@@ -0,0 +1,16 @@
+//
+//  Nutrient.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-30.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import Foundation
+
+/**
+	Wrapper class or enum, representing a specific nutrient.
+**/
+enum Nutrient {
+	
+}
