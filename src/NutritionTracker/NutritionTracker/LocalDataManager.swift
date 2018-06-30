77b30e82b81b8561a68a6a680db2af0f78762686
alc29 <alc29@sfu.ca>
2018-06-29 17:50:23 -0700

    add LocalDataManager.swift

---
+++ src/NutritionTracker/NutritionTracker/LocalDataManager.swift
@@ -0,0 +1,31 @@
+//
+//  LocalDataManager.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-06-28.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+//TODO only 1 instance should exist at a time.
+class LocalDataManager {
+//	@objc private dynamic var userRealm?
+	
+//	init() {
+//		//TODO load userRealm
+//	}
+//
+//	func saveMeal(newMeal: String) { //TODO Meal Class
+//		let realm = try! Realm()
+//		try!realm.write {
+//			//TODO add new Meal to list of user's meals
+//			//realm.add(newMeal)
+//		}
+//	}
+//
+//	func getMeals() {
+//
+//	}
+}
