cbca5dde37d061ee07c3ccac56fbe2319c2395db
alc29 <alc29@sfu.ca>
2018-07-01 23:01:09 -0700

    add FoodSearchBar.swif

---
+++ src/NutritionTracker/NutritionTracker/FoodSearchBar.swift
@@ -0,0 +1,21 @@
+//
+//  FoodSearchBar.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-07-01.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import UIKit
+
+class FoodSearchBar: UISearchBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ src/NutritionTracker/NutritionTracker/FoodSearchDisplayController.swift
@@ -0,0 +1,13 @@
+//
+//  FoodSearch.swift
+//  NutritionTracker
+//
+//  Created by alc29 on 2018-07-01.
+//  Copyright © 2018 alc29. All rights reserved.
+//
+
+import UIKit
+
+class FoodSearch: UISearchDisplayController {
+
+}
