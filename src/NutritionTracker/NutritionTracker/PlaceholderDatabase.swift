a2b7561e080b28b634768d723c41083951221509
alc29 <alc29@sfu.ca>
2018-07-02 15:24:10 -0700

    add todo

--- src/NutritionTracker/NutritionTracker/PlaceholderDatabase.swift
+++ src/NutritionTracker/NutritionTracker/PlaceholderDatabase.swift
@@ -14,6 +14,7 @@ import RealmSwift
 
 /**
 Fake database for testing, until integration with actual.
+//TODO rename to Database
 */
 class PlaceholderDatabase {
 	static let sharedInstance = PlaceholderDatabase()
