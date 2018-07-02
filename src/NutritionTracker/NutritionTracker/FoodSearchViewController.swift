82ec5fb460aef7ff7be213922726796cf82fbf37
alc29 <alc29@sfu.ca>
2018-07-02 14:23:06 -0700

    cleanup

--- src/NutritionTracker/NutritionTracker/FoodSearchViewController.swift
+++ src/NutritionTracker/NutritionTracker/FoodSearchViewController.swift
@@ -1,35 +1,122 @@
 //
-//  FoodSearchViewController.swift
+//  FoodSearchViewController2.swift
 //  NutritionTracker
 //
-//  Created by alc29 on 2018-06-30.
+//  Created by alc29 on 2018-07-02.
 //  Copyright © 2018 alc29. All rights reserved.
 //
 
 import UIKit
 
-class FoodSearchViewController: UIViewController {
-
+class FoodSearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	//MARK: Properties
+	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var searchFooter: SearchFooterView!
+	
+	var foodDetailController: FoodDetailViewController? = nil
+	var results = [FoodItem]()
+	var filteredResults = [FoodItem]()
+	let searchController = UISearchController(searchResultsController: nil)
+	
+	//MARK: - View Setup
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // setup search controller
+		searchController.searchResultsUpdater = self
+		searchController.obscuresBackgroundDuringPresentation = false
+		searchController.searchBar.placeholder = "Search Foods"
+		navigationItem.searchController = searchController
+		definesPresentationContext = true
+		
+		//setup the scope bar
+		searchController.searchBar.scopeButtonTitles = ["All", "Meat", "Vegges"]
+		searchController.searchBar.delegate = self
+		
+		//setup search footer
+		tableView.tableFooterView = searchFooter
+		
+		//fake list of results
+		for i in 0..<10 {
+			results.append(FoodItem(i, "Food item \(i)"))
+		}
+		
+//		if let splitViewController = splitViewController {
+//			let controllers = splitViewController.viewControllers
+//			foodDetailController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? FoodDetailViewController
+//		}
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+	override func viewWillAppear(_ animated: Bool) {
+//		if splitViewController!.isCollapsed {
+//			if let selectionIndexPath = tableView.indexPathForSelectedRow {
+//				tableView.deselectRow(at: selectionIndexPath, animated: animated)
+//			}
+//		}
+		super.viewWillAppear(animated)
+	}
+	
+	// MARK: = Table View
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		if isFiltering() {
+			searchFooter.setIsFilteringToShow(filteredItemCount: filteredResults.count, of: results.count)
+			return filteredResults.count
+		}
+		searchFooter.setNotFiltering()
+		return results.count
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		let foodItem: FoodItem
+		if isFiltering() {
+			foodItem = filteredResults[indexPath.row]
+		} else {
+			foodItem = results[indexPath.row]
+		}
+		cell.textLabel!.text = foodItem.getName()
+		cell.detailTextLabel!.text = "todo candy.category"
+		return cell
+	}
+	
+	//MARK: - Segues
+	
+	func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+		filteredResults = results.filter({(foodItem: FoodItem) -> Bool in
+			let doesCategoryMatch = (scope == "All") || (true) //|| (candy.category == scope)
+			if searchBarIsEmpty() {
+				return doesCategoryMatch
+			} else {
+				return doesCategoryMatch && foodItem.getName().lowercased().contains(searchText.lowercased())
+			}
+		})
+		tableView.reloadData()
+	}
+	func searchBarIsEmpty() -> Bool {
+		return searchController.searchBar.text?.isEmpty ?? true
+	}
+	func isFiltering() -> Bool {
+		let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
+		return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
+	}
+	
+}
 
-    /*
-    // MARK: - Navigation
+extension FoodSearchViewController: UISearchBarDelegate {
+	//MARK: - UISearchBar Delegate
+	func searchBar(_ searchBar: UISearchBar, selectedScopeButotnIndexDidChange selectedScope: Int) {
+		filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+	}
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension FoodSearchViewController: UISearchResultsUpdating {
+	// MARK: - UISearchResultsUpdating Delegate
+	func updateSearchResults(for searchController: UISearchController) {
+		let searchBar = searchController.searchBar
+		let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+		filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+	}
 }
