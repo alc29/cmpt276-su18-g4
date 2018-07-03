cefd192423635b40c70017c4e1ea13c0a99f1288
alc29 <alc29@sfu.ca>
2018-07-03 00:16:53 -0700

    add comments & impl unstructions

--- src/NutritionTracker/NutritionTracker/PlaceholderDatabase.swift
+++ src/NutritionTracker/NutritionTracker/PlaceholderDatabase.swift
@@ -20,21 +20,34 @@ class PlaceholderDatabase {
 	static let sharedInstance = PlaceholderDatabase()
 	private init() {}
 	
-	//let amount =  PlaceholderDatabase.sharedInstance.getAmountOf(nutrient: nutrient, fromFoodId: foodId);
-	func getAmountOf(nutrient: Nutrient, fromFoodId: Int) -> Amount {
-		//TODO return standard amount of the nutrient that corresponds to the food (foodId)
-		return Amount(Float(arc4random_uniform(10)))
-	}
 	
-	func getAmountPerOf(nutrient: Nutrient, fromFoodId: Int) -> AmountPer {
-		//TODO return the proper AmountPer
+	
+	//Return how much of the given nutrient is contained in the food corresponding to foodId
+	func getAmountPerOf(nutrient: Nutrient, foodId: Int) -> AmountPer {
+		/* TODO
+		using the foodId, retrieve the corresponding food item, or other relevant info, from the database
+		determine the amount of the desired nutrient contained in this food
+		create a new AmountPer() using this nutrient data
+		return the value
+		*/
 		return AmountPer() //TODO replace placeholder
 	}
 	
+	// Query the database with the given string, and return an array of FoodItem's.
 	func search(_ searchTerms: String) -> [FoodItem] {
+		/* TODO
+		var array = [FoodItem]()
+		query database using searchTerms
+		convert the returned json into the proper structs
+		convert the structs into FoodItem's
+		add the FoodItems to the array
+		return array
+		*/
 		return testSearch()
 	}
 	
+	
+	//used for testing - ignore
 	private func testSearch() -> [FoodItem] {
 		var items = [FoodItem]()
 		for i in 0...12 {
@@ -43,14 +56,30 @@ class PlaceholderDatabase {
 		return items
 	}
 	
-//	func search(category: ) {
-//	}
+	//return a list of food items corresponding to the given food group, used to populate Food Group Categories in the catalog, for browsing.
+	//func search(foodGroup: FoodGroup) -> [FoodItem] {
+		/* TODO
+			see FoodGroup.swift
+			use foodGroup.name to get the foodgroup's name
+			or foodGroup.id to get its id
+			return an array of FoodItems that correspond to the foodgroup.
+			this is just used to create a list for the user to browse. we can limit it to, say, the first 50, and decide later how to filter results.
 	
-	//return the nutrients in the given food
-	func getNutrients(fromFoodId: Int) -> [FoodItemNutrient] {
-		//return an array of nutrients corresponding to the given foodId
+			later on we'd probably want the same functionality, but for Nutrients instead of FoodGroups. this could be another function, if you have the time.
+		*/
+	//}
+
+	
+	//given a specific food (id), return a list of the nutrients that the food contains.
+	func getNutrients(foodId: Int) -> [FoodItemNutrient] {
 		var nutrients = [FoodItemNutrient]()
-		//TODO get food from database, and create list of nutrients.
+		
+		/* TODO
+			retrieve the FoodItem that cooresponds to the given foodId
+			return a list of the nutrients that the food contains.
+			aka return an array of FoodItemNutrient's.
+			see below for examples.
+		*/
 		
 		//TEST - for now return a fake list of nutrients.
 		nutrients.append(FoodItemNutrient(Nutrient.TestBitterNutrientA, AmountPer()))
